212564582cb16ae33908137425340fe8fd2dc26f
Jarkko Linnanvirta <jarkko@taitavasti.fi>
2018-11-07 20:39:43 +0200

    jquery-eu-cookie-law-popup.js: Remove default settings and some demo code. Move the default settings to eucookielawpopup.yml.

--- vendor/wimagguc/jquery-eu-cookie-law-popup/js/jquery-eu-cookie-law-popup.js
+++ vendor/wimagguc/jquery-eu-cookie-law-popup/js/jquery-eu-cookie-law-popup.js
@@ -28,18 +28,6 @@ $.fn.euCookieLawPopup = (function() {
 	///////////////////////////////////////////////////////////////////////////////////////////////
 	// PARAMETERS (MODIFY THIS PART) //////////////////////////////////////////////////////////////
 	_self.params = {
-		cookiePolicyUrl : '/?cookie-policy',
-		popupPosition : 'top',
-		colorStyle : 'default',
-		compactStyle : false,
-		popupTitle : 'This website is using cookies',
-		popupText : 'We use cookies to ensure that we give you the best experience on our website. If you continue without changing your settings, we\'ll assume that you are happy to receive all cookies on this website.',
-		buttonContinueTitle : 'Continue',
-		buttonLearnmoreTitle : 'Learn&nbsp;more',
-		buttonLearnmoreOpenInNewWindow : true,
-		agreementExpiresInDays : 30,
-		autoAcceptCookiePolicy : false,
-		htmlMarkup : null
 	};
 
 	///////////////////////////////////////////////////////////////////////////////////////////////
@@ -248,16 +236,6 @@ $.fn.euCookieLawPopup = (function() {
 	};
 
 	return publicfunc;
-});
-
-$(document).ready( function() {
-	if ($(".eupopup").length > 0) {
-		$(document).euCookieLawPopup().init({
-			'info' : 'YOU_CAN_ADD_MORE_SETTINGS_HERE',
-			'popupTitle' : 'This website is using cookies. ',
-			'popupText' : 'We use them to give you the best experience. If you continue using our website, we\'ll assume that you are happy to receive all cookies on this website.'
-		});
-	}
 });
 
 $(document).bind("user_cookie_consent_changed", function(event, object) {
